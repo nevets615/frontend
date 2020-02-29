c122e345edc9b500a45a6d9836a7d653d937dbb0
nevets615 <surj316@yahoo.com>
2020-02-29 14:58:46 -0500

    created a fifferent file structure for chat

--- src/components/chat/chat.js
+++ src/components/chat/chat.js
@@ -1,46 +1,22 @@
 import React, { Component } from "react";
 import "./chat.css";
 
-class ChatDisplay extends Component {
-  state = {
-    message: ""
-  };
-
-  handleMessageInput = e => {
-    e.persist();
-    const feed = document.getElementById("feed");
-
-    if (e.target.value) {
-      this.setState({ message: e.target.value }, () => {
-        feed.scrollTop = feed.scrollHeight;
-      });
-    }
-    if (e.key && e.key === "Enter") {
-      this.props
-        .onSpeak(this.state.message)
-        .then(() => {
-          this.setState({ message: "" });
-          feed.scrollTop = feed.scrollHeight;
-        })
-        .catch(() => this.setState({ message: "" }));
-    }
-  };
+class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: DUMMY_DATA
+    };
+  }
 
   render() {
-    const { messageFeed, onSpeak } = this.props;
-    const { message } = his.state;
-
     return (
-      <div>
-        <div className="Text">
-          value={message}
-          onChange={this.handleMessageInput}
-          onKeyUp={this.handleMessageInput}
-        </div>
-        <button type="submit" onClick={onSpeak}></button>
+      <div className="app">
+        <MessageList messages={this.state.messages} />
+        <SendMessageForm />
       </div>
     );
   }
 }
 
-export default ChatDisplay;
+export default Chat;
